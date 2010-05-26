95328efb3a0b330ec08338f5375efe1393fc31f7
hiroponz <sathiroyuki@gmail.com>
2010-05-26 17:35:42 +0900

    Update readme.

--- init.rb
+++ init.rb
@@ -27,13 +27,13 @@ RAILS_DEFAULT_LOGGER.info 'Starting Customer plugin for RedMine'
 
 Redmine::Plugin.register :redmine_customer do
   name 'Customer plugin'
-  author 'Eric Davis'
+  author 'Hiroyuki Sato'
   description 'This is a plugin for Redmine that can be used to track basic customer information'
-  version '0.2.0'
+  version '0.3.0'
   requires_redmine :version_or_higher => '0.9.0'
 
-  url 'https://projects.littlestreamsoftware.com/projects/redmine-customers' if respond_to? :url
-  author_url 'http://www.littlestreamsoftware.com' if respond_to? :author_url
+  url 'http://hiroponz.net/redmine/projects/priw' if respond_to? :url
+  author_url 'http://hiroponz.net/redmine/projects/priw' if respond_to? :author_url
 
   project_module 'customer' do
     permission :manage_customers, :customers => [:new, :create, :edit, :update, :destroy]
